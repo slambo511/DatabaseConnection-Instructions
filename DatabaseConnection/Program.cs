30f8b1f8daba39fa4f023430d3ae37ea1ad79090
Samuel Bancroft <samuelbancroft@hotmail.com>
2019-01-06 00:56:34 +0000

    tested and working examples for next project.

--- DatabaseConnection/Program.cs
+++ DatabaseConnection/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.Data.SqlClient;
 
 namespace DatabaseConnection
 {
@@ -6,7 +8,125 @@ namespace DatabaseConnection
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                // Build connection string
+                var builder = new SqlConnectionStringBuilder
+                {
+                    DataSource = @"DESKTOP-T74VM4I\SERVER17PASS",
+                    ConnectTimeout = 30,
+                    Encrypt = true,
+                    TrustServerCertificate = false,
+                    IntegratedSecurity = true,
+                    ApplicationIntent = ApplicationIntent.ReadWrite,
+                    MultiSubnetFailover = false
+                };
+                // update me
+
+                // Connect to SQL
+                Console.Write("Connecting to SQL Server ...");
+                using (var connection = new SqlConnection(builder.ConnectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Done.");
+
+                    // Create a sample database
+                    Console.Write("Dropping and creating database 'SampleDB' ...");
+                    var sql = "DROP DATABASE IF EXISTS [SampleDB]; CREATE DATABASE [SampleDB]";
+                    using (var command = new SqlCommand(sql, connection))
+                    {
+                        command.ExecuteNonQuery();
+                        Console.WriteLine("Done");
+                    }
+
+                    // Create a table and insert some sample data
+                    Console.Write("Creating a sample table with data, press any key to continue...");
+                    Console.ReadKey(true);
+                    var sb = new StringBuilder();
+                    sb.Append("USE SampleDB; ");
+                    sb.Append("CREATE TABLE Employees ( ");
+                    sb.Append(" Id INT IDENTITY(1,1) NOT NULL PRIMARY KEY, ");
+                    sb.Append(" Name NVARCHAR(50), ");
+                    sb.Append(" Location NVARCHAR(50) ");
+                    sb.Append("); ");
+                    sb.Append("INSERT INTO Employees (Name, Location) VALUES ");
+                    sb.Append("(N'Jared', N'Australia'), ");
+                    sb.Append("(N'Nikita', N'India'), ");
+                    sb.Append("(N'Tom', N'Germany'); ");
+                    sql = sb.ToString();
+                    using (var command = new SqlCommand(sql, connection))
+                    {
+                        command.ExecuteNonQuery();
+                        Console.WriteLine(("Done."));
+                    }
+
+                    // INSERT demo
+                    Console.Write("Inserting a new row into table, press any key to continue...");
+                    Console.ReadKey(true);
+                    sb.Clear();
+                    sb.Append("INSERT Employees (Name, Location) ");
+                    sb.Append("VALUES (@name, @location);");
+                    sql = sb.ToString();
+                    using (var command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@name", "Jake");
+                        command.Parameters.AddWithValue("@location", "United States");
+                        var rowsAffected = command.ExecuteNonQuery();
+                        Console.WriteLine(rowsAffected + " row(s) inserted.");
+                    }
+
+                    // UPDATE demo
+                    var userToUpdate = "Nikita";
+                    Console.Write("Updating 'Location' for user '" + userToUpdate + "', press any key to continue...");
+                    Console.ReadKey(true);
+                    sb.Clear();
+                    sb.Append("UPDATE Employees SET Location = N'United States' WHERE Name = @Name");
+                    sql = sb.ToString();
+                    using (var command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@name", userToUpdate);
+                        var rowsAffected = command.ExecuteNonQuery();
+                        Console.WriteLine(rowsAffected + " row(s) updated");
+                    }
+
+                    // DELETE demo
+                    var userToDelete = "Jared";
+                    Console.Write("Deleting user '" + "', press any key to continue...");
+                    Console.ReadKey(true);
+                    sb.Clear();
+                    sb.Append("DELETE FROM Employees WHERE Name = @name;");
+                    sql = sb.ToString();
+                    using (var command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@name", userToDelete);
+                        var rowsAffected = command.ExecuteNonQuery();
+                        Console.WriteLine(rowsAffected + " row(s) deleted");
+                    }
+
+                    // READ demo
+                    Console.WriteLine("Reading data from table, press any key  to continue...");
+                    Console.ReadKey(true);
+                    sql = "SELECT Id, Name, Location FROM Employees;";
+                    using (var command = new SqlCommand(sql, connection))
+                    {
+                        using (var reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("{0} {1} {2}", reader.GetInt32(0), reader.GetString(1), reader.GetString(2));
+                            }
+                        }
+                    }
+
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("All done. Press any key to finish...");
+            Console.ReadKey(true);
         }
     }
 }
